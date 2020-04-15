a8e4f6ecbff27ea9a3218551040792aa99da82f2
Paulo Cesar Ramos <cesarprh@gmail.com>
2020-04-15 14:07:44 -0400

    Intermediate question 1

---
+++ InterQ1.R
@@ -0,0 +1,14 @@
+########################
+### Nivel Intermedio ###
+########################
+###    Question 3    ###
+########################
+
+# limpiar la data 
+rm(list = ls())
+
+# Invocar las librerias a utilizar
+library(quantmod)
+library(PerformanceAnalytics)
+library(tseries)
+
