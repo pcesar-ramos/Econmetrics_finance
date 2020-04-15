7996c938e13f9d248d6f76ca4548cdf39e81228b
Paulo Cesar Ramos <cesarprh@gmail.com>
2020-04-15 14:32:20 -0400

    Save

--- InterQ1.R
+++ InterQ1.R
@@ -10,5 +10,29 @@ rm(list = ls())
 # Invocar las librerias a utilizar
 library(quantmod)
 library(PerformanceAnalytics)
+# Para el test de raiz unitaria ADF
 library(tseries)
+
+# Descargando datos de Yahoo Finance
+# periodicity = 'monthly'
+
+# Coca cola / Indice bursatil de alemania / Tipo e cambio EURUSD#
+getSymbols.yahoo('KO', env = globalenv(), return.class = "xts",
+                 from = '2010-04-20', to = Sys.Date(),
+                 periodicity = 'monthly')
+
+getSymbols.yahoo('DAX', env = globalenv(), return.class = "xts",
+                 from = '2010-04-20', to = Sys.Date(),
+                 periodicity = 'monthly')
+
+getSymbols.yahoo('EURUSD=X', env = globalenv(), return.class = "xts",
+                 from = '2010-04-20', to = Sys.Date(),
+                 periodicity = 'monthly')
+# Cambiar de nombre la Variable
+EURUSD = `EURUSD=X`
+
+par(mfrow=c(2,1))
+chart.TimeSeries(KO$KO.Close, main = 'APPLE_Close_Price')
+chart.TimeSeries(DAX$DAX.Close, main = 'APPLE_Close_Price')
+chart.TimeSeries(EURUSD$`EURUSD=X.Close`, main = 'APPLE_Close_Price')
 
