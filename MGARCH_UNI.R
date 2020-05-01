39212333517e4df286aa6e8318a42b1dc25194c1
Paulo Cesar Ramos <cesarprh@gmail.com>
2020-04-30 21:27:55 -0400

    Apuntes

---
+++ MGARCH_UNI.R
@@ -0,0 +1,16 @@
+# Econometría Finnaciera
+# Modelo GARCH multivariado Garch
+# Diversificación del riesgo
+# Eje x : Riesgo - Eje y: Retorno
+# Curva e Markowitz (Frontera eficiente)
+# Portafolios sobre la curva son eficiente
+# La línea de mercado de capital (CAL)
+# El óptimo portafolio es mCAL=mCE
+# Ratio = Rentabilidad(y)/Riesgo(x) tenemos un esoeciue de pendiente
+# Ret Cateto Opuesto Risk cateto adiacente
+# Premio Nobel Markowitz - Sharpe contribuyo con un ratio (El ratio de sharpe)
+# Rs = (E(Rp)-Rf )/ Desv standar del portafolio Rf tasa libre de riesgo
+# Rf es el activo "libre de riesgo" Tiene que estar en el eje y porque el riesgo es 0 es una intersección en el eje y
+# El asset allocation: cuanto distribuyo por cada activo en mi portafolio
+
+# Vamos al R studio
